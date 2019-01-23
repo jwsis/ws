ddf36ee35e793c1443f7791067093681e5218fa7
jwsis <41461310+jwsis@users.noreply.github.com>
2019-01-23 19:07:55 +0800

    Update freeread-00-init-dist.js

--- ts-p99-freeread/freeread-00-init-dist.js
+++ ts-p99-freeread/freeread-00-init-dist.js
@@ -1,35 +1,22 @@
-let tool_freeread_ui =
-    (function () {
-        return <frame>
-            <ScrollView>
-                <vertical>
-                <text text="====视频类====" marginTop="10" textColor="blue" gravity="center" typeface="monospace" />
-                </vertical>
-            </ScrollView>
-        </frame>
-    })();
 
+let tool_freeread_ui =
+    <ScrollView>
+        <vertical>
+            <text text="====说明====" marginTop="10" textColor="blue" gravity="center" typeface="monospace" />
+        </vertical>
+    </ScrollView>;
 
 (function () {
+
     let code = "tool_freeread";
-    let mth = threads.start(function () {
 
-        try {
-            ctx.ts[code] = {
-                th: mth,
-                ui: ui.inflate(tool_freeread_ui),
-                run: function () { },
-                init: () => {
-
-                }
-            };
-
-        } catch (e) {
+    ctx.ts[code] = {
+        ui: ui.inflate(tool_intro_ui),
+        run: function () {
+            alert("请选择其他工具运行");
+        },
+        init: () => {
+            
         }
-    });
-
-    setTimeout(() => {
-        // ui.layout(tool_freeread_ui);
-    }, 1000);
-
+    };
 })();
