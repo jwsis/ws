b1561947c117e3b758c15dfb925ff964a0303a2b
jwsis <41461310+jwsis@users.noreply.github.com>
2019-01-23 19:00:35 +0800

    Update freeread-00-init-dist.js

--- ts-p99-freeread/freeread-00-init-dist.js
+++ ts-p99-freeread/freeread-00-init-dist.js
@@ -1,10 +1,9 @@
-"ui";
 let tool_freeread_ui =
     (function () {
         return <frame>
             <ScrollView>
                 <vertical>
-
+                <text text="====视频类====" marginTop="10" textColor="blue" gravity="center" typeface="monospace" />
                 </vertical>
             </ScrollView>
         </frame>
@@ -12,22 +11,25 @@ let tool_freeread_ui =
 
 
 (function () {
+
     let mth = threads.start(function () {
 
         try {
             ctx.ts[code] = {
                 th: mth,
                 ui: ui.inflate(tool_freeread_ui),
-                run: function () { 
-                    toastLog(111)
-                },
+                run: function () { },
                 init: () => {
-                    
+
                 }
             };
 
         } catch (e) {
         }
     });
+
+    setTimeout(() => {
+        // ui.layout(tool_freeread_ui);
+    }, 1000);
 
 })();
